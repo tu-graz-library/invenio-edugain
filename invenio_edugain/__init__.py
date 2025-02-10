19a604d007067b567ffcd99c4573af47f573798f
Martin Obersteiner <obersteiner@tugraz.at>
2025-02-10 14:24:46 +0100

    add metadata for building python distributions
    this differs from other invenio-modules' build-metadata in that:
    - moved declarative metadata from `setup.cfg` to `pyprojec.toml`
    - `wheel` is no longer a build-requirement for `setuptools`
    - `zip-safe`-flag was deprecated by `setuptools`
    - `include-package-data = true` isn't specified as it is now the default

---
+++ invenio_edugain/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 Graz University of Technology.
+#
+# invenio-edugain is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"Invenio extension for login via edugain."
+
+__version__ = "0.0.1"
+
+__all__ = ("__version__",)
