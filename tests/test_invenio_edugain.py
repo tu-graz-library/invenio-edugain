b693102fa68183f1a8af95e30711bf2e130ced53
Martin Obersteiner <obersteiner@tugraz.at>
2025-02-19 11:15:24 +0100

    add testing via `pytest`

---
+++ tests/test_invenio_edugain.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 Graz University of Technology.
+#
+# invenio-edugain is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Module tests."""
+
+
+def test_version():
+    """Test for existence of `__version__` attribute."""
+    from invenio_edugain import __version__
+
+    assert __version__
