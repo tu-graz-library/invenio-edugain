fe565dcee49197bca4335f3bdae671cf5b6dac4d
Martin Obersteiner <obersteiner@tugraz.at>
2025-03-12 13:49:31 +0100

    tests: add testing of data-ingestion

--- run-tests.sh
+++ run-tests.sh
@@ -10,7 +10,13 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 
 # Usage
-#   ./run-tests.sh [pytest options and args...]
+#   ./run-tests.sh [-K|--keep-services] [pytest options and args...]
+#
+# Note: you can chose which db-service to run tests with via environment-variable `DB`
+#       if not set, DB=postgresql is used as default
+#
+# Example for testing with mysql instead of postgresql:
+#    DB=mysql ./run-tests.sh
 
 # Quit on errors
 set -o errexit
@@ -19,10 +25,32 @@ set -o errexit
 set -o nounset
 
 # Check for arguments
+keep_services=0
 pytest_args=()
 for arg in $@; do
-    pytest_args+=( ${arg} )
+    # from the CLI args, filter out some known values and forward the rest to "pytest"
+    # note: we don't use "getopts" here b/c of some limitations (e.g. long options),
+    #       which means that we can't combine short options (e.g. "./run-tests -Kk pattern")
+    case ${arg} in
+        # Note: lower-case `-k` would clash with "pytest"
+        -K|--keep-services)
+            keep_services=1
+            ;;
+        *)
+            pytest_args+=( ${arg} )
+            ;;
+    esac
 done
 
+# if not --keep-services, then down services on exit
+function cleanup {
+    eval "$(docker-services-cli down --env)"
+}
+if [[ ${keep_services} -eq 0 ]]; then
+    trap cleanup EXIT
+fi
+
+
 ruff check .
+eval "$(docker-services-cli up --db ${DB:-postgresql} --env)"
 python -m pytest ${pytest_args[@]+"${pytest_args[@]}"}
---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 Graz University of Technology.
+#
+# invenio-edugain is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Pytest configuration."""
+
+import pytest
+from invenio_app.factory import create_app as invenio_create_app
+
+
+@pytest.fixture(scope="module")
+def app_config(app_config: dict):
+    """Expand default app-config."""
+    return app_config
+
+
+@pytest.fixture(scope="module")
+def create_app():
+    """Flask app fixture for invenio_edugain."""
+    return invenio_create_app
