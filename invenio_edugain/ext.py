bcc50eef11d4e525547d4d2caa71b6520fdc37cc
Martin Obersteiner <obersteiner@tugraz.at>
2025-04-08 11:03:58 +0200

    ext: register package as flask-extension

---
+++ invenio_edugain/ext.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 Graz University of Technology.
+#
+# invenio-edugain is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Flask-extension setup for invenio-edugain."""
+
+from flask import Flask
+
+
+class InvenioEdugain:
+    """invenio-edugain flask-extension."""
+
+    def __init__(self, app: Flask | None = None) -> None:
+        """Extension initialization."""
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app: Flask) -> None:
+        """Flask application initialization."""
+        app.extensions["invenio-edugain"] = self
