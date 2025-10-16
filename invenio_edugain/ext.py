66186a2ad5eaa9d880c9c29f6febd86c6a2288c4
Martin Obersteiner <obersteiner@tugraz.at>
2025-10-16 12:49:35 +0200

    config: parse paths to crypto key-files from env

--- invenio_edugain/ext.py
+++ invenio_edugain/ext.py
@@ -30,3 +30,15 @@ class InvenioEdugain:
         for k in dir(config):
             if k.startswith("EDUGAIN_"):
                 app.config.setdefault(k, getattr(config, k))
+
+        pysaml2_config = app.config["EDUGAIN_PYSAML2_CONFIG"]
+        if signing_key := app.config.get("EDUGAIN_SIGNING_KEY"):
+            pysaml2_config["key_file"] = signing_key
+        if signing_cert := app.config.get("EDUGAIN_SIGNING_CRT"):
+            pysaml2_config["cert_file"] = signing_cert
+
+        encryption_config = pysaml2_config["encryption_keypairs"]
+        if encryption_key := app.config.get("EDUGAIN_ENCRYPTION_KEY"):
+            encryption_config["key_file"] = encryption_key
+        if encryption_cert := app.config.get("EDUGAIN_ENCRYPTION_CRT"):
+            encryption_config["cert_file"] = encryption_cert
