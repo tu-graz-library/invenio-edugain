6f2211516bf4b1814ba72e37b21de2e92c84301d
Martin Obersteiner <obersteiner@tugraz.at>
2025-12-03 11:46:51 +0100

    db: update models to distinguish visible/enabled
    also remove unused rows `logo_url` and `displayname`

--- invenio_edugain/models.py
+++ invenio_edugain/models.py
@@ -18,14 +18,8 @@ class IdPData(db.Model):
     __tablename__ = "edugain_idp_data"
 
     id: Mapped[str] = mapped_column(primary_key=True)
-    # TODO: differentiate between displayname and search-string
-    #       if org behind IdP has multiple alternate names:
-    #          - can only display one name
-    #          - but all names should be findable when searching with disco service
-    # TODO: language-preference for displaynames
-    displayname: Mapped[str]
+    discoverable: Mapped[bool] = mapped_column(default=True)
     enabled: Mapped[bool] = mapped_column(default=False)
-    logo_url: Mapped[str]
     # holds internal (i.e. already parsed by pysaml2) representation of idp-settings
     settings: Mapped[dict] = mapped_column(
         db.JSON().with_variant(JSONB(), "postgresql"),
@@ -36,8 +30,7 @@ class IdPData(db.Model):
         return (
             f"{type(self).__qualname__}("
             f"id={self.id!r}, "
-            f"displayname={self.displayname!r}, "
+            f"discoverable={self.discoverable!r}, "
             f"enabled={self.enabled!r}, "
-            f"logo_url={self.logo_url!r}, "
             "settings=...)"
         )
